df80209556a84c772fd0125b1ddc9b05dff9fda7
Adish Sagarawat <69076256+Adish08@users.noreply.github.com>
2025-02-24 20:17:43 +0530

    Hide Badge

---
+++ Framer_Badge.tsx
@@ -0,0 +1,10 @@
+    const style = `
+  #__framer-badge-container {
+    display: none; 
+  }
+`
+    return (
+        <div>
+            <style dangerouslySetInnerHTML={{ __html: style }} />
+        </div>
+    )
